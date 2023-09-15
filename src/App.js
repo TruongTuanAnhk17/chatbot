24416e015dcaaa9e42db60bf289192546d1c7187
Truong Tuan Anh <truongtuananhkk@gmail.com>
2023-09-15 10:07:44 +0700

    login facebook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from "./logo.svg";
 import "./App.css";
 import MessengerCustomerChat from "react-messenger-customer-chat";
+import { LoginSocialFacebook } from "reactjs-social-login";
+import { FacebookLoginButton } from "react-social-login-buttons";
+import { useState } from "react";
 
 function App() {
+  const [profile, setProfile] = useState(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {!profile ? (
+        <LoginSocialFacebook
+          appId="1308091889820994"
+          onResolve={(res) => {
+            console.log(res);
+            setProfile(res.data);
+          }}
+          onReject={(err) => {
+            console.log(err);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <FacebookLoginButton />
+        </LoginSocialFacebook>
+      ) : (
+        <>
+          <h1>{profile.name}</h1>
+          <img alt="" src={profile.picture.data.url} />
+        </>
+      )}
       <MessengerCustomerChat
         pageId="132677533257382"
         appId="1308091889820994"
